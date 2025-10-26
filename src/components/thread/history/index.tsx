61b84ccd1d482a65060445fd6cd9238df052fc90
liduchuan <alex.liduchuan@gmail.com>
2025-10-26 23:37:21 +0800

    fix: reorder imports and disable exhaustive-deps rule in ESLint config

--- src/components/thread/history/index.tsx
+++ src/components/thread/history/index.tsx
@@ -3,8 +3,6 @@ import { useThreads } from "@/providers/Thread";
 import { Thread } from "@langchain/langgraph-sdk";
 import { useEffect } from "react";
 
-import { getContentString } from "../utils";
-import { useQueryState, parseAsBoolean } from "nuqs";
 import {
   Sheet,
   SheetContent,
@@ -12,8 +10,10 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { Skeleton } from "@/components/ui/skeleton";
-import { PanelRightOpen, PanelRightClose } from "lucide-react";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { PanelRightClose, PanelRightOpen } from "lucide-react";
+import { parseAsBoolean, useQueryState } from "nuqs";
+import { getContentString } from "../utils";
 
 function ThreadList({
   threads,
